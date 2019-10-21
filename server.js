385874a18dc37c2d270fd03f1075071e1360950f
Amir Reza Dalir <dalirnet@gmail.com>
2019-10-21 14:12:54 +0330

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { status, render, json } = require('server/reply');
 const figlet = require('figlet');
 const request = require('request-promise');
 const _ = require('lodash');
+const extractDomain = require('extract-domain');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const args = require('minimist')(process.argv.slice(2), {
@@ -18,6 +19,47 @@ const args = require('minimist')(process.argv.slice(2), {
   }
 });
 
+const link = {
+  type(link, baseDomain) {
+    // [internal,external,social,void]
+    if (link.indexOf('http://') < 0 && link.indexOf('https://') < 0) {
+      return 'void';
+    } else {
+      let domain = extractDomain(link);
+      if (_.indexOf(['facebook.com', 'instagram.com', 'google.com', 'aparat.com', 'twitter.com', 't.me', 'telegram.me'], domain) > -1) {
+        return 'social';
+      } else if (domain != baseDomain) {
+        return 'external';
+      }
+    }
+
+    return 'internal';
+  },
+  anchor(html) {
+    let anchor = "";
+    _.forEach(html.getElementsByTagName('img'), (img) => {
+      if (img.getAttribute("alt")) {
+        anchor = img.getAttribute("alt");
+        return false;
+      }
+    });
+    if (anchor == "") {
+      if (html.getAttribute("title")) {
+        anchor = html.getAttribute("title");
+      } else {
+        anchor = html.textContent;
+      }
+    }
+    return anchor.replace(/\s\s+/g, ' ').trim();
+  },
+  rel(rel) {
+    if (!rel) {
+      return 'follow';
+    }
+    return rel;
+  }
+};
+
 figlet('Link', (err, data) => {
   if (!err && !args.debug) {
     console.log(data);
@@ -26,24 +68,74 @@ figlet('Link', (err, data) => {
     port: args.port,
     views: 'template',
     public: 'public',
-    engine: 'html',
+    engine: 'hbs',
     favicon: 'public/icon.png'
   }, [
     get('/', ctx => {
-      return render('index.html');
+      return render('form.hbs', { name: 'amir' });
     }),
-    post('/', ctx => request.get(ctx.data).then((html) => {
-      const { document } = (new JSDOM(html)).window;
-
-      return json(words);
-    }).catch((err) => {
-      console.error(err.message);
-      return status(400);
-    }))
+    get('/fetch', ctx => {
+      let data = {
+        url: ctx.query.url,
+        title: ctx.query.url,
+        link: {
+          internal: [],
+          external: [],
+          social: [],
+          void: []
+        },
+        domain: [],
+        repeat: {
+          link: [],
+          anchor: []
+        }
+      };
+      return request.get(encodeURI(ctx.query.url)).then((html) => {
+        const baseDomain = extractDomain(ctx.query.url);
+        const { document } = (new JSDOM(html)).window;
+        // parse page
+        data.title = document.getElementsByTagName('title')[0].innerHTML;
+        _.forEach(document.getElementsByTagName('a'), (item) => {
+          let href = decodeURI(item.getAttribute('href') ? item.getAttribute('href') : "");
+          if (href) {
+            let domain = (extractDomain(href) ? extractDomain(href) : baseDomain);
+            let type = link.type(href, baseDomain);
+            let anchor = link.anchor(item);
+            let rel = link.rel(item.getAttribute('rel'));
+            //
+            data.link[type].push({ href, domain, anchor, rel });
+            if (type == "external") {
+              data.domain.push(domain);
+            }
+            if (type == "internal") {
+              data.repeat.link.push(href);
+              data.repeat.anchor.push(anchor);
+            }
+          }
+        });
+        data.domain = _.countBy(data.domain);
+        data.repeat.link = _.fromPairs(_.reverse(_.sortBy(_.toPairs(_.pickBy(_.countBy(data.repeat.link), (v, i) => { return v > 2 })), 1)));
+        data.repeat.anchor = _.fromPairs(_.reverse(_.sortBy(_.toPairs(_.pickBy(_.countBy(data.repeat.anchor), (v, i) => { return v > 1 })), 1)));
+        data.count = {
+          link: {
+            internal: data.link.internal.length,
+            external: data.link.external.length,
+            social: data.link.social.length,
+            void: data.link.void.length
+          },
+          domain: _.size(data.domain),
+          repeat: {
+            link: _.size(data.repeat.link),
+            anchor: _.size(data.repeat.anchor)
+          }
+        };
+        return render('report.hbs', data);
+      }).catch((err) => {
+        console.error(err.message);
+        return render('form.hbs', { error: "خطا!" });
+      });
+    })
   ]);
-  if (typeof (process.argv[0])) {
-
-  }
   if (!args.debug) {
     open('http://localhost:' + args.port);
   }
